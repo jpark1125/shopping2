eefcec52e9c4568bcb4e5be26222c952e123113e
jpark1125 <pjy3886@naver.com>
2024-03-19 23:05:23 +0900

    socket server +

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const { server_boot } = require("./app");
-
+const { socket_boot } = require("./socket");
 const { admin_boot } = require("./admin/admin");
 server_boot();
 admin_boot();
+socket_boot();
 
 console.log(`server on ${new Date().toString()} port 3000`);
 console.log(`server on ${new Date().toString()} port 3001`);
+console.log(`server on ${new Date().toString()} port 3002`);
---
+++ socket.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const app = express();
+
+const util = require("./utils");
+const Router = require("./routes");
+const client = require("./middleware/redis.conn");
+require("dotenv").config();
+
+//socket 관련부분
+const { createServer } = require("node:http");
+const { join } = require("node:path");
+const { Server } = require("socket.io");
+const server = createServer(app);
+const io = new Server(server);
+
+io.on("connection", (socket) => {
+  console.log("새로운 클라이언트가 연결되었습니다.");
+
+  socket.on("chat message", (message) => {
+    console.log("받은 메시지:", message);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("클라이언트가 연결을 끊었습니다.");
+  });
+});
+
+//여기까지 socket
+
+app.get("/", (req, res) => {
+  res.sendFile(join(__dirname, "index.html"));
+});
+
+const socket_boot = async () => {
+  server.listen(3002, "0.0.0.0");
+};
+
+module.exports = { socket_boot };
